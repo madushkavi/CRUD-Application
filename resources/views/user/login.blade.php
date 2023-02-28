f4228f0e2b87079f59b4e18352eada712ffb9adb
unknown <madushkavithanawasam148@gmail.com>
2023-02-28 23:44:02 +0530

    Updated Home Page

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,20 +1,22 @@
+@extends('interface.layout')
+@section('content')
 <div class="row">
- <div class="col-sm-8 offset-sm-2">
-    <h1 class="display">Laravel Login Application</h1><br/>
+    <div class="col-sm-8 offset-sm-2">
+        @if(isset(Auth::user()->email))
+        <script>
+            window.location = "user/intro";
+        </script>
+        @endif
 
-    @if(isset(Auth::user()->email))
-    <script>window.location="/success";</script>
-    @endif
-
-    @if ($message = Session::get('error'))
+        @if ($message = Session::get('error'))
         <div class="alert alert-danger">
             <button type="button" class="close" data-dismiss="alert">x</button>
-			<strong>{{ $message }}</strong>
+            <strong>{{ $message }}</strong>
         </div>
         @endif
 
 
-    @if ($errors->any())
+        @if ($errors->any())
         <div class="alert alert-danger">
             <ul>
                 @foreach ($errors->all() as $error)
@@ -24,21 +26,27 @@
         </div><br />
         @endif
 
-	<div>
-      <form method="post" action="{{ url('/done') }}"> 
-          {{ csrf_field() }}
-          <div class="form-group">    
-              <label for="email">Email Id:</label>
-              <input type="email" class="form-control" name="email"/>
-          </div>
-          <div class="form-group">
-              <label for="password">Password:</label>
-              <input type="password" class="form-control" name="password"/>
-          </div>
-          <div class="form-group">              
-            <button type="submit" class="btn btn-primary-outline">Login</button>
-          <div>
-      </form>
-  </div>
+        <div class="row">
+            <div class="col-sm-8 offset-sm-2">
+                <h1 class="display">LOGIN </h1></br>
+                <div>
+                    <form method="post" action="{{url('/done')}}">
+                        {{csrf_field()}}
+                        <div class="form-group">
+                            <label for="email">E-mail: </label>
+                            <input type="email" class="form-control" name="email">
+                        </div>
+                        <div class="form-group">
+                            <label for="password">Password: </label>
+                            <input type="password" class="form-control" name="password">
+                        </div><br>
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-primary">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-</div>
+@endsection('content')
